96322469a00e8dc3581aa3b03462c5778df9bf67
SumaiyaNishat <sumaiyanishat94@gmail.com>
2026-08-26 23:50:04 +0600

    updated

---
+++ src/app/api/feedback/[id]/route.js
@@ -0,0 +1,7 @@
+export async function GET(request , { params }) {
+    const { id } = await params;
+
+    const singleFeedback = feedback.find((fb) => fb.id ==id) || {};
+
+    return Response.json(singleFeedback);
+}
--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,3 +1,27 @@
+import { feedback } from "../route";
+
 export async function GET(request) {
-    
+    return Response.json(feedback);
+}
+export async function POST(request) {
+
+    const {message} = await request.json();
+
+    if(!message || typeof message !== "string") {
+        return Response.json({
+            status: 400,
+            message: "Message is required"
+        });
+    }
+
+    const newFeedback = {message, id: feedback.length + 1};
+    feedback.push(newFeedback);
+
+    return Response.json({
+        // status: 200,
+        // message: "Feedback submitted successfully",
+        // data
+        acknowledged: true,
+        insertedId: newFeedback.id,
+    });
 }
